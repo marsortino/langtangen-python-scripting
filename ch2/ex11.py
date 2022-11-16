81463905f75187422494b6c33b1853abf1c859ee
marsortino <118361726+marsortino@users.noreply.github.com>
2022-11-16 16:44:18 +0100

    Delete ex11.py

--- ch2/ex11.py
+++
@@ -1,6 +0,0 @@
-# Extend the simviz1.py script such that it writes an HTML file containing
-# the values of the physical and numerical parameters, a sketch of the system
-# (src/py/misc/figs/simviz2.xfig.t.gif is a suitable file), and a PNG plot of
-# the solution. In case you are not familiar with writing HTML code, I have
-# made a quick introduction, particularly relevant for this exercise, in the file
-# src/misc/html-intro/oscillator.html
